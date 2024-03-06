453bc09d8ace60266f912c5a8d8052bcff988301
robo7660 <ssmoser+bytingirish@gmail.com>
2024-03-05 20:32:59 -0500

    Increase acceleration of bot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,9 +25,9 @@ public final class Constants {
   public static final boolean kRightFrontDriveInverted = false;
 
   public static final double LOOP_TIME = 0.13;
-  public static final double ROBOT_MASS = 150 * 0.453592;
+  public static final double ROBOT_MASS = 115 * 0.453592;
   public static final Matter CHASSIS =
-      new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+      new Matter(new Translation3d(0, 0, Units.inchesToMeters(0)), ROBOT_MASS);
 
   public static final PIDConstants autoTranslationPID = new PIDConstants(0.7, 0, 0);
   public static final PIDConstants autoRotationPID = new PIDConstants(0.4, 0, 0.01);
